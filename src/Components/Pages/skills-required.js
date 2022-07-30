713902f088ecb8365cf90890f70073540322140a
KelvinBM <kelvinmejia182@gmail.com>
2022-07-30 16:18:31 +0000

    made some changes in skills required page and added some things

--- src/Components/Pages/skills-required.js
+++ src/Components/Pages/skills-required.js
@@ -3,7 +3,7 @@ import '../../css/skills-required.css';
 
 function SkillsRequired() {
     return(
-        <div>
+        <div className= "body">
             <h1 id= "skills-top-header">
                 Introduction to Soft
                 skills and Technical skills
@@ -53,7 +53,7 @@ function SkillsRequired() {
 
             </p>
             <h3 id= "soft-skill-cards-header">Examples of Soft Skills</h3>
-            <div className="soft-skill-cards" >
+            <div className="soft-skill-cards" id="card-1">
                 <div className="card">
                     <img
                         src="https://champsoftblog.com/wp-content/uploads/2020/08/time_management_hero-1-970x679-1.png"
@@ -69,28 +69,43 @@ function SkillsRequired() {
                     </div>
                 </div>
             </div>
-
-            <div className="soft-skill-cards" >
-            <div className="card">
-                <img
-                    src="https://bevynkhanal.files.wordpress.com/2019/10/active-listening.jpg"
-                    className="card-img-top"
-                    alt="Active listening"
-                />
-                <div className="card-zbody">
-                    <p className="card-text">
-                        <font size="2">
+            <div className="soft-skill-cards" id="card-1">
+                <div className="card">
+                    <img
+                        src="https://miro.medium.com/max/1400/1*_ii8hbx9b9DhBV1gD87VEw.jpeg"
+                        className="card-img-top"
+                        alt="Adaptability"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
                         <div className="shadow p-3 mb-5 bg-body rounded">
-                            <b>Active Listening - </b>Rather than giving someone a fraction
-                            of your attention, active listening is making a conscious effort
-                            to hear, understand, and retain information that's being relayed
-                            to you. It involves more than listening to the words they say.
+                            <b>Adaptability - </b> the quality of being able to adjust to new conditions.
                         </div>
-                        </font>
-                    </p>
+                    </div>
                 </div>
             </div>
-            <div className="soft-skill-cards">
+            <div className="soft-skill-cards" id= "card-1">
+                <div className="card">
+                    <img
+                        src="https://bevynkhanal.files.wordpress.com/2019/10/active-listening.jpg"
+                        className="card-img-top"
+                        alt="Active listening"
+                    />
+                    <div className="card-zbody">
+                        <p className="card-text">
+                            <font size="3">
+                            <div className="shadow p-3 mb-5 bg-body rounded">
+                                <b>Active Listening - </b>Rather than giving someone a fraction
+                                of your attention, active listening is making a conscious effort
+                                to hear, understand, and retain information that's being relayed
+                                to you. It involves more than listening to the words they say.
+                            </div>
+                            </font>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="soft-skill-cards" id="card-1">
                 <div className="card">
                         <img
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMU9_-3LhUGHKNCDe_JYwmKY_wXqMrE9kb7A&usqp=CAU"
@@ -109,28 +124,26 @@ function SkillsRequired() {
                     </div>
                 </div>
             </div>
-            </div>
-
-            <div className="soft-skill-cards">
-            <div className="card">
-                <img
-                    src="https://i.ytimg.com/vi/t8MP731UJvE/maxresdefault.jpg"
-                    className="card-img-top"
-                    alt="Critical Thinking"
-                />
-                <div className="card-body">
-                    <p className="card-text"></p>
-                    <div className="shadow p-3 mb-5 bg-body rounded" id="critical-thinking">
-                        <b>Critical Thinking - </b>Using creativity and foresight to achieve
-                        tasks and reach goals in the most efficient ways. Mastering critical
-                        thinking often requires a slowed-down pace, where an employee
-                        considers all parties involved and all possible outcomes before
-                        making a decision.
+            <div className="soft-skill-cards" id= "card-1">
+                <div className="card">
+                    <img
+                        src="https://i.ytimg.com/vi/t8MP731UJvE/maxresdefault.jpg"
+                        className="card-img-top"
+                        alt="Critical Thinking"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded" id="critical-thinking">
+                            <b>Critical Thinking - </b>Using creativity and foresight to achieve
+                            tasks and reach goals in the most efficient ways. Mastering critical
+                            thinking often requires a slowed-down pace, where an employee
+                            considers all parties involved and all possible outcomes before
+                            making a decision.
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-            <div className="soft-skill-cards">
+            <div className="soft-skill-cards" id= "card-1">
                 <div className="card">
                     <img
                         src="https://blog.vantagecircle.com/content/images/2019/04/team-building-activities.png"
@@ -149,7 +162,7 @@ function SkillsRequired() {
                 </div>
             </div>
 
-            <div className="soft-skill-cards">
+            <div className="soft-skill-cards" id="card-1">
                 <div className="card">
                     <img
                         src="https://cdn.glitch.global/46db144e-f9f6-412f-a38c-c8931e7c7c5e/9b06cc97-7fa4-4d37-b8ea-9d46d58acf9b.image.png?v=1657813692137"
@@ -167,28 +180,192 @@ function SkillsRequired() {
                     </div>
                 </div>
             </div>
-
-            <img
-                id="hardskill"
-                className="technicalskillsimg"
-                src="https://cdn.glitch.global/46db144e-f9f6-412f-a38c-c8931e7c7c5e/download.png?1657810713484"
-                alt="technical Skills"
-            />
-
-            <div className="soft-skill-cards">
+            <div className="soft-skill-cards" id="card-1">
                 <div className="card">
                         <img
                         src="https://www.insperity.com/wp-content/uploads/improve_accountability_1200x630-2.png"
                         className="card-img-top"
                         alt="Dependability"
                         />
-                    <div className="card-body">
+                    <div className="card-body" >
                         <p className="card-text"></p>
                         <div className="shadow p-3 mb-5 bg-body rounded" >
                             <font size="3">
                                 <b>Personal Accountability - </b> To acknowledge your mistakes, accept 
                                 responsibility for them and to figure out how to fix them.
                             </font>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h3 id="technical-skills-header">Technial/Hard Skills</h3>
+            <p id="paragraphs-1">
+                Technical Skills are specialized knowledge and expertise required to
+                perform specific tasks and they require the use of specific tools and programs 
+                in real world situations. Diverse technical skills are required in just about 
+                every field and industry, from IT and business administration to health care and
+                education.
+            </p>
+            <p id="paragraphs-2">
+                In fact, many entry-level positions across industries require basic
+                technical skills, such as cloud computing in Google Drive and navigating
+                social media platforms. Examples of more advanced technical skills that a
+                job might require include programming, debugging, or
+                data analysis.
+            </p>
+            <p id="paragraphs-1">
+                Technical skills include programming, technical writing, coding, 
+                programming languages and algorithms.
+            </p>            
+            <p id= "paragraphs-2">
+                Technical skills are extremely useful, 
+                and being good at coding, programming and algorithms
+                are crucial if you want to pursue a career in computer science.
+            </p>
+            <p id="paragraphs-1">
+                Not only arethese skills useful for computer science but for other jobs too. 
+                An example could be engineering, they use technical skills for most of the jobs they
+                do.
+            </p>
+            <p id="paragraphs-2">
+                Debugging is also an important technical skill and all debugging is, is
+                going back and fixing any errors or problems with your code this just
+                makes your code work more efficiently.
+            </p>
+            <h3 id= "hard-skill-cards-header">Examples of Hard Skills</h3>
+            <div className="hard-skill-cards" id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://cdn.glitch.global/46db144e-f9f6-412f-a38c-c8931e7c7c5e/thumbnails%2Farticle-3467-thumbnail.png?1657724924446"
+                        className="card-img-top"
+                        alt="algorithms"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Algorithms</b> - a set of instructions used by computers to solve
+                            a given problem in order to meet an end goal. learning algorithms is
+                            crucial to design and develop efficient computer programs.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://media-exp1.licdn.com/dms/image/C5612AQHMIRehTvRcHg/article-cover_image-shrink_600_2000/0/1520150075767?e=1659571200&v=beta&t=g2fGi_-J5bRH-MoF6exdGvTwl3gUTlAMrSi6hknapvY"
+                        className="card-img-top"
+                        alt="Technical writing"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Technical Writing</b> - any writing designed to explain 
+                            complex, technical, and specialized information to audiences 
+                            who may or may not be familiar with them.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://cdn.glitch.global/46db144e-f9f6-412f-a38c-c8931e7c7c5e/thumbnails%2Finfotech-courses.jpg?1657721561201"
+                        className="card-img-top"
+                        alt="programming"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Programming</b> - there are many jobs today that require
+                            programming knowledge. If you’re looking for a job as a writer, for
+                            example, you may be required to have some basic knowledge of HTML.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                    src="https://y26uq11r8xr1zyp0d3inciqv-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/35-1-1024x597.jpg"
+                    className="card-img-top"
+                    alt="analysing data"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Data analysis</b> - being able to interpret and analyze complex
+                            data.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://www.anques.com/wp-content/uploads/2019/07/software-development.jpg"
+                        className="card-img-top"
+                        alt="Software Development"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Software Development </b> - the process of working with others to
+                            write code and programs, troubleshoot and test them, and ultimately
+                            create a software program that’s usable.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://d1rytvr7gmk1sx.cloudfront.net/wp-content/uploads/2022/04/kanban-board-concept-042022-1-1.jpg?x27457"
+                        className="card-img-top"
+                        alt="Project Management"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Project Management </b> - the application of processes, methods, skills, 
+                            knowledge and experience to achieve specific project objectives according 
+                            to the project acceptance criteria within agreed parameters.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://www.thebalancecareers.com/thmb/QIM-djypVHy3XJCxv8MODvF05cY=/735x0/computer-skills-list-2063738-Final-d30001660f21447aa6e14d29c21c6a83.png"
+                        className="card-img-top"
+                        alt="Software Proficiency"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Software Proficiency </b> - the application of processes, methods, skills, 
+                            knowledge and experience to achieve specific project objectives according 
+                            to the project acceptance criteria within agreed parameters.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="hard-skill-cards"  id= "card-2">
+                <div className="card">
+                    <img
+                        src="https://www.netsolutions.com/insights/wp-content/uploads/2021/12/11-most-popular-programming-languages.webp"
+                        className="card-img-top"
+                        alt="Programming Languages"
+                    />
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <div className="shadow p-3 mb-5 bg-body rounded">
+                            <b>Programming Languages </b> - any set of rules that converts strings, or 
+                            graphical program elements in the case of visual programming languages, to 
+                            various kinds of machine code output. Programming languages are one kind of 
+                            computer language, and are used in computer programming to implement algorithms.
                         </div>
                     </div>
                 </div>
