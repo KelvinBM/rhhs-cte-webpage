4d848627ef937a10bc5b2abf81409f936d98d55b
KelvinBM <kelvinmejia182@gmail.com>
2022-08-01 15:27:40 +0000

    added home changes from Nand and changes from Dylan on the SEP academy

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -7,16 +7,21 @@ class Home extends Component {
     return (
       <div>
         <Splash/>
-        <h2 id= "homeH2"> Career and Technical Education programs are made up of elective courses that teach students about different careers. Richmond Hill High School offers several different CTE pathways including: </h2>
-        <ul>
-            <li className= "list1">Hospitalisty and Tourism</li>
-            <li className= "list1">Software Engineering</li>
-            <li className= "list1">Health Sciences</li>
-            <li className= "list1">IT and Robotics</li>
-            <li className= "list1">Art and Design</li>
-            <li className= "list1">Criminology and Forensics</li>
-        </ul>
-        <h3 id= "homeH3">Explore each of our pathways above to learn about what we do at Richmond Hill!</h3>
+        <div id= "body" >
+            <h2 id= "homeH2"> Career and Technical Education programs are made up of elective courses that teach students about different careers. Richmond Hill High School offers several different CTE pathways including: </h2>
+            <ul className = "list">
+                <li><a href="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932678&type=d&pREC_ID=2066003">Hospitalisty & Tourism</a></li>
+                <li><a href="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932698&type=d&pREC_ID=2065998">Software Engineering</a></li>
+                <li><a href="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932673&type=d&pREC_ID=2066028">Health Sciences</a></li>
+                <li><a href="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932679&type=d&pREC_ID=2066019">IT, Enginnering & Robotics</a></li>
+                <li><a href="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932672&type=d&pREC_ID=2066008">Art & Design</a></li>
+                <li><a href="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932671&type=d&pREC_ID=2065993">Criminology and Forensics</a></li>
+            </ul>
+            <h3 id="homeH3">Explore each of our pathways above to learn about what we do at Richmond Hill!</h3>
+            <h4 id= "homeH4"> Watch one of our videos below : </h4>
+            <iframe id = "video"  src="https://www.youtube.com/embed/RIJGUwAQxDU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+
+        </div>
       </div>
     );
   }
